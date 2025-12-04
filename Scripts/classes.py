22ef820e1860b82a59a9257bf91af21b5a9b248f
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2025-12-04 15:51:26 +0200

    def Matrix_create

--- Scripts/classes.py
+++ Scripts/classes.py
@@ -5,13 +5,23 @@ from matplotlib import pyplot as plt
 
 
 class Graph:
-    def __init__(self, max_edges, density):
-        self.vertices = max_edges
+    def __init__(self, vertices, max_edges, density):
+        self.vertices = vertices
+        self.max_edges = max_edges
         self.density = density
         self.vertex_list = []
     def graph_draw(self):
         graphs_edges = self.vertices * self.density # if n 100 graph_edges = 990
         return print(graphs_edges)
+    def matrix_create(self):
+        with open("Matrix.txt", "w") as matrix:
+            matrix.write("  ")
+            for i in range(1, self.vertices + 1):
+                matrix.write(f"{i}".rjust(3))
+            for i in range(1, self.vertices + 1):
+                matrix.write(f"\n{i} ".rjust(4)+"|")
 
     def dfs_check(self):
         pass
+graph = Graph(10, 90, 10)
+graph.matrix_create()
