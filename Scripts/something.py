ef3b2314eb49dbe9310592896a9819e07be8b889
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2025-12-04 16:24:58 +0200

    script something.py

---
+++ Scripts/something.py
@@ -0,0 +1,32 @@
+from classes import *
+import matplotlib
+from collections import namedtuple
+try:
+    vertices = int(input("Enter how many vertices Graph has: "))
+    density = float(input("Enter percents of Graph density: "))/100
+    max_edges = vertices * (vertices-1)
+except ValueError:
+    print("You need a number")
+
+nodes = []
+density_edges = density * max_edges
+Graph = namedtuple("Graph", ["nodes", "edges"])
+for i in range(1, vertices):
+    nodes.append(f"a{i}")
+edges = [
+    ("a1", "a2"),
+    ("a2", "a1"),
+    ("a3", "a4"),
+    ("a1", "a5"),
+    ("a4", "a1"),
+    ("a5", "a4"),
+]
+G = Graph(nodes, edges)
+def adjacency_dict(graph):
+    adj = {node: [] for node in graph.nodes}
+    for edge in graph.edges:
+        node1, node2 = edge[0], edge[1]
+        adj[node1].append(node2)
+        adj[node2].append(node1)
+    return adj
+print(adjacency_dict(G))
