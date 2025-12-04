a3f8ecdcdc19124184465e5f00ca781d8eb18b83
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2025-12-04 17:29:40 +0200

    update something.py

--- Scripts/something.py
+++ Scripts/something.py
@@ -1,32 +1,55 @@
-from classes import *
-import matplotlib
-from collections import namedtuple
-try:
-    vertices = int(input("Enter how many vertices Graph has: "))
-    density = float(input("Enter percents of Graph density: "))/100
-    max_edges = vertices * (vertices-1)
-except ValueError:
-    print("You need a number")
+import random as rd
 
-nodes = []
-density_edges = density * max_edges
-Graph = namedtuple("Graph", ["nodes", "edges"])
-for i in range(1, vertices):
-    nodes.append(f"a{i}")
-edges = [
-    ("a1", "a2"),
-    ("a2", "a1"),
-    ("a3", "a4"),
-    ("a1", "a5"),
-    ("a4", "a1"),
-    ("a5", "a4"),
-]
-G = Graph(nodes, edges)
-def adjacency_dict(graph):
-    adj = {node: [] for node in graph.nodes}
-    for edge in graph.edges:
-        node1, node2 = edge[0], edge[1]
-        adj[node1].append(node2)
-        adj[node2].append(node1)
-    return adj
-print(adjacency_dict(G))
+
+class Matrix:
+    def __init__(self, size: int):
+        self.__size = size
+        self.matrix_list = []
+        for i in range(self.__size):
+            self.matrix_list.append([])
+            for j in range(self.__size):
+                self.matrix_list[i].append(0)
+
+    def get_size(self):
+        return self.__size
+
+    def __str__(self):
+        result = ""
+        for i in range(self.get_size()):
+            for j in range(self.get_size()):
+                result += str(self.matrix_list[i][j]) + "|"
+            result += "\n"
+        return result
+
+    def randomize(self):
+        for i in range(self.get_size()):
+            for j in range(self.get_size()):
+                if i == j:
+                    continue
+                self.matrix_list[i][j] = rd.randint(0, 1)
+
+
+def multiply(m1: Matrix, m2: Matrix):
+    size = m1.get_size()
+    if size != m2.get_size():
+        return
+    resmat = Matrix(size)
+    for i in range(size):
+        for j in range(size):
+            pass
+
+
+m1 = Matrix(5)
+m1.randomize()
+print(m1)
+print(m1.matrix_list)
+dict_adjacency = {}
+lst_adjacency = []
+def adjacency_list():
+    for i in range(0, len(m1.matrix_list)):
+
+        dict_adjacency.setdefault(f"v{i}", m1.matrix_list[i])
+    return print(dict_adjacency)
+
+    print(dict_adjacency)
+adjacency_list()
