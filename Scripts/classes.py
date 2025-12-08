94e7c486ebb4be5745041e0deaacc0874d72d057
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2025-12-08 13:38:28 +0200

    cleared folder

--- Scripts/classes.py
+++ Scripts/classes.py
@@ -13,15 +13,8 @@ class Graph:
     def graph_draw(self):
         graphs_edges = self.vertices * self.density # if n 100 graph_edges = 990
         return print(graphs_edges)
-    def matrix_create(self):
-        with open("Matrix.txt", "w") as matrix:
-            matrix.write("  ")
-            for i in range(1, self.vertices + 1):
-                matrix.write(f"{i}".rjust(3))
-            for i in range(1, self.vertices + 1):
-                matrix.write(f"\n{i} ".rjust(4)+"|")
+
 
     def dfs_check(self):
         pass
 graph = Graph(10, 90, 10)
-graph.matrix_create()
