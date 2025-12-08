799308ab4f19d5f1d98a55487b87ca49c8b434dd
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2025-12-08 13:52:09 +0200

    updte main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -12,26 +12,6 @@ except ValueError:
     print("You need a number")
 
 density_edges = density * max_edges
-Graph = namedtuple("Graph", ["nodes", "edges"])
-
-nodes = ["A","B","C","D"]
-edges = [
-    ("A", "B"),
-    ("A", "B"),
-    ("A", "C"),
-    ("A", "C"),
-    ("B", "D"),
-    ("C", "D"),
-]
-G = Graph(nodes, edges)
-def adjacency_dict(graph):
-    adj = {node: [] for node in graph.nodes}
-    for edge in graph.edges:
-        node1, node2 = edge[0], edge[1]
-        adj[node1].append(node2)    
-        adj[node2].append(node1)
-    return adj
-print(adjacency_dict(G))
 
 m = MatrixGraph(vertices, density)
 print(kahn(m))
