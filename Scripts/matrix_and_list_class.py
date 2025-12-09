694256b038f5b58303d9d3e8e7a36fec5e2f3f89
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2025-12-09 21:21:55 +0200

    new ListGraph

--- Scripts/matrix_and_list_class.py
+++ Scripts/matrix_and_list_class.py
@@ -89,84 +89,93 @@ class ListGraph:
         return self.__size
 
     def get_edges(self) -> int:
-        result = 0
-        for v in self.list_adjacency:
-            result += sum(v)
-        return result
+        return sum(len(lst) for lst in self.adj_list.values())
 
     def __str__(self) -> str:
-        dict_adjacency = {}
-        lst_of_vertices = []
-        for i in range(0, len(self.list_adjacency)):
-            for j in range(0, len(self.list_adjacency)):
-                if self.list_adjacency[i][j] == 1:
-                    lst_of_vertices.append(f"v{j}")
-            dict_adjacency.setdefault(f"v{i}", lst_of_vertices)
-            lst_of_vertices = []
+        result = ""
+        for v, lst in self.adj_list.items():
+            result += f"{v}: {lst}\n"
+        return result
 
-        return f"Список суміжності: {dict_adjacency}"
 
-    # DFS-based cycle detection algorithm from https://en.wikipedia.org/wiki/Cycle_(graph_theory)
-    def __dfs(self, v_number: int, visited_list: list, finished_list: list) -> bool:
-
-        if v_number in finished_list:
+    def __dfs(self, v: int, visited: set, finished: set) -> bool:
+        if v in finished:
             return False
-        if v_number in visited_list:
+        if v in visited:
             return True
-        visited_list.append(v_number)
-        result = 0
-        for i in range(self.get_size()):
-            if self.list_adjacency[v_number][i] == 1:
-                result += int(self.__dfs(i, visited_list, finished_list))
-        finished_list.append(v_number)
-        return bool(result)
+
+        visited.add(v)
+        cycle = False
+
+        for u in self.adj_list[v]:
+            if self.__dfs(u, visited, finished):
+                cycle = True
+
+        finished.add(v)
+        return cycle
 
     def detect_cycle(self) -> bool:
-        result = 0
-        visited_list = []
-        finished_list = []
-        for i in range(self.get_size()):
-            if i in visited_list:
-                visited_list.remove(i)
-            if i in finished_list:
-                finished_list.remove(i)
-            result += int(self.__dfs(i, visited_list, finished_list))
-        return bool(result)
+        visited = set()
+        finished = set()
 
-    # Erdos-Renyi-based random graph generation
-    def __randomize(self, density: float) -> None:
-        graph_size = self.get_size()
-        full_graph_edges = graph_size * (graph_size - 1)
-        generate_edges = int(full_graph_edges * density)
-        for i in range(generate_edges * 10):
+        for v in range(self.get_size()):
+            if v in visited:
+                visited.remove(v)
+            if v in finished:
+                finished.remove(v)
+
+            if self.__dfs(v, visited, finished):
+                return True
+
+        return False
+
+
+    def __randomize(self, density: float):
+        n = self.get_size()
+        full_edges = n * (n - 1)
+        generate_edges = int(full_edges * density)
+
+        attempts = generate_edges * 10
+
+        for _ in range(attempts):
             if generate_edges <= 0:
                 return
-            v1, v2 = rd.randint(0, graph_size - 1), rd.randint(0, graph_size - 1)
-            if self.list_adjacency[v1][v2] == 1:
+
+            v1 = rd.randint(0, n - 1)
+            v2 = rd.randint(0, n - 1)
+
+            if v1 == v2:
                 continue
-            self.list_adjacency[v1][v2] = 1
+
+            if v2 in self.adj_list[v1]:
+                continue
+            self.adj_list[v1].append(v2)
             if self.detect_cycle():
-                self.list_adjacency[v1][v2] = 0
+                self.adj_list[v1].remove(v2)
             else:
                 generate_edges -= 1
+
         else:
             print("Given density is too high to generate a graph without cycles. \nSo we have generated graph with max possible density")
 
-    def __init__(self, size: int, density: float, copy=None) -> None:
-        if copy == None:
+    def __init__(self, size: int, density: float, copy=None):
+        if copy is None:
             self.__size = size
         else:
             self.__size = copy.get_size()
-        self.list_adjacency = []
-        for i in range(self.get_size()):
-            self.list_adjacency.append([])
-            for j in range(self.get_size()):
-                if copy == None:
-                    self.list_adjacency[i].append(0)
-                else:
-                    self.list_adjacency[i].append(copy.list_adjacency[i][j])
-        if copy == None:
+
+        self.adj_list = {i: [] for i in range(self.get_size())}
+
+        if copy is not None:
+
+            for v in range(self.get_size()):
+                self.adj_list[v] = list(copy.adj_list[v])
+        else:
+
             self.__randomize(density)
+
+m = ListGraph(10, 0.1)
+print(m)
 
 
 
